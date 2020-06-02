fd1565794978dc82c5a545ccc55f2e3feaab7665
captain13128 <capitan.develop@gmail.com>
2020-06-02 13:40:21 +0300

    change kovan-addresses.json

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
 from chief_keeper.chief_keeper import ChiefKeeper
 
+RPC_HOST = "https://kovan.infura.io/v3/683836c8b9384898a9f99d483ae389bc"
+NETWORK = "kovan"
+
+ETH_FROM = "0xC0CCab7430aEc0C30E76e1dA596263C3bdD82932"
+KEY_FILE = "/home/captain/development/dss-deploy-scripts/keystore.json"
+PASS_FILE = "/home/captain/development/dss-deploy-scripts/p.pass"
+
+ADDRESSES_FILE = "/home/captain/development/makerdao_python/chief-keeper/addresses/kovan-addresses.json"
+
 
 if __name__ == '__main__':
     flip_args = [
-        '--rpc-host', 'https://kovan.infura.io/v3/683836c8b9384898a9f99d483ae389bc',
-        '--eth-from', '0xC0CCab7430aEc0C30E76e1dA596263C3bdD82932',
-        '--network', 'kovan',
-        '--eth-key', 'key_file=/home/captain/development/dss-deploy-scripts/keystore.json,pass_file=/home/captain/development/dss-deploy-scripts/p.pass',
-        '--dss-deployment-file', '/home/captain/development/makerdao_python/chief-keeper/addresses/kovan-addresses.json',
+        '--rpc-host', RPC_HOST,
+        '--eth-from', ETH_FROM,
+        '--network', NETWORK,
+        '--eth-key', f'key_file={KEY_FILE},pass_file={PASS_FILE}',
+        '--dss-deployment-file', ADDRESSES_FILE,
         '--chief-deployment-block', '17707858',
-        '--debug'
+        # '--debug'
     ]
     ChiefKeeper(flip_args).main()
